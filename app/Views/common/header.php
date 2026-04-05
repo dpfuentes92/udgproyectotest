1c53a7832d2111fbeb41f3ba393d4919b7473de3
leonelmartinez8296-LMP <leonel.martinez8296@alumnos.udg.mx>
2026-04-05 14:49:27 -0600

    RF1-Implementación-MóduloEnvío

--- app/Views/common/header.php
+++ app/Views/common/header.php
@@ -3,6 +3,9 @@
 <title><?= $title ?? 'UDG-Proyectos' ?></title>
 <!-- Bootstrap 5 CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<!-- Bootstrap Icons -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+
 <!-- Custom CSS -->
 <style>
     :root {
